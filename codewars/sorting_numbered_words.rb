5c7fdb39701d7f960b30f9daf1a8f87a0feb8398
Chris Geekie <chris.geekie@gmail.com>
2021-03-23 12:49:33 +0100

    Added one liner

--- codewars/sorting_numbered_words.rb
+++ codewars/sorting_numbered_words.rb
@@ -28,6 +28,9 @@ def order(words)
   ordered_string.join(' ')
 end
 
+# One liner
+#  words.split.sort_by{ |w| w[/\d/] }.join(' ')
+
 puts 'Running tests...'
 assert_equal(order('is2 Thi1s T4est 3a'), 'Thi1s is2 3a T4est')
 assert_equal(order('4of Fo1r pe6ople g3ood th5e the2'), 'Fo1r the2 g3ood 4of th5e pe6ople')
