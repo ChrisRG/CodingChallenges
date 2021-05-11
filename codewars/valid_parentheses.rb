0c1443b24936a0cff2a198a3dba80cf4d8716e6f
Chris Geekie <chris.geekie@gmail.com>
2021-05-11 11:51:10 +0200

    Solved

--- codewars/valid_parentheses.rb
+++ codewars/valid_parentheses.rb
@@ -13,12 +13,33 @@
 # Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters. Furthermore, the input string may be empty and/or not contain any parentheses at all. Do not treat other forms of brackets as parentheses (e.g. [], {}, <>).
 
 def valid_parentheses(string)
+  stack = []
+  
+  string.strip.chars.each do |char|
+    if char == ')'
+      return false unless stack.last == '('
 
+      stack.pop
+    elsif char == '('
+      stack.push(char)
+    end
+  end
+  stack.empty?
 end
 
 class Test
-  def assert_equals(left, right)
-
+  def self.assert_equals(left, right)
+    result = left == right ? "PASSED" : "FAILED"
+    puts result
+    if result == "FAILED"
+      puts "Expected: #{right}"
+      puts "Got: #{left} \n\n"
+    end
   end
 end
 
+Test.assert_equals(valid_parentheses("  ("),false)
+Test.assert_equals(valid_parentheses(")test"),false)
+Test.assert_equals(valid_parentheses(""),true)
+Test.assert_equals(valid_parentheses("hi())("),false)
+Test.assert_equals(valid_parentheses("hi(hi)()"),true)
