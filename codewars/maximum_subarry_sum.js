ea2bdf3d02f595d70ef506154690bb1051d92e35
Chris Geekie <chris.geekie@gmail.com>
2021-05-12 12:26:17 +0200

    Wrong solution

--- codewars/maximum_subarry_sum.js
+++ codewars/maximum_subarry_sum.js
@@ -8,6 +8,36 @@
 // Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
 const maxSequence = (arr) => {
-  // sort the array
-  //
+  const sort_arr = arr.sort((a, b) => a - b);;
+  console.log(sort_arr);
+  let list = []
+  let sub_arr;
+  let left_ptr = 0;
+  let right_ptr; 
+
+  while (left_ptr < arr.length) {
+    sub_arr = [sort_arr[left_ptr]];
+    right_ptr = left_ptr + 1;
+    while (sort_arr[right_ptr] === sort_arr[right_ptr - 1] + 1) {
+      sub_arr.push(sort_arr[right_ptr]);
+      right_ptr += 1; 
+    }
+    list.push(sub_arr);
+    left_ptr = right_ptrt;
+  }
+  console.log(list);
 }
+
+class Test {
+  static assertEquals(left, right) {
+    const result = (left === right ? "PASSED" : "FAILED" );
+    console.log(result);
+    if (result === "FAILED") {
+      console.log(`EXPECTED ${left}`);
+      console.log(` TO EQUAL ${right}`);
+    }
+  }
+}
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+// Test.assertEquals(maxSequence([]), 0);
+// Test.assertEquals(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
