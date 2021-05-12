552aa9a2f90909b4860949ea839ac52ee156bfe3
Chris Geekie <chris.geekie@gmail.com>
2021-05-12 11:40:40 +0200

    Added solution + tests

--- codewars/string_ends_with.js
+++ codewars/string_ends_with.js
@@ -5,4 +5,20 @@
 // solution('abc', 'bc') // returns true
 // solution('abc', 'd') // returns false
 
+function solution(str, ending) {
+  return str.slice(0 - ending.length, str.length) === ending
+}
 
+class Test {
+  static assertEquals(left, right) {
+    const result = (left === right ? "PASSED" : "FAILED" );
+    console.log(result);
+    if (result === "FAILED") {
+      console.log(`EXPECTED: ${right}`);
+      console.log(`GOT: ${left}`);
+    }
+  }
+}
+
+Test.assertEquals(solution('abcde', 'cde'), true)
+Test.assertEquals(solution('abcde', 'abc'), false)
