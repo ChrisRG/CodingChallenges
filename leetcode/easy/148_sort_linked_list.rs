3b8e00810c61a05af11fe52b324e96c34e21eada
Chris Geekie <chris.geekie@gmail.com>
2021-06-03 18:55:45 +0200

    Added sorted list problem

---
+++ leetcode/easy/148_sort_linked_list.rs
@@ -0,0 +1,24 @@
+// Given the head of a linked list, return the list after sorting it in ascending order.
+
+// Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ #[derive(PartialEq, Eq, Clone, Debug)]
+ pub struct ListNode {
+   pub val: i32,
+   pub next: Option<Box<ListNode>>
+ }
+ 
+ impl ListNode {
+   #[inline]
+   fn new(val: i32) -> Self {
+     ListNode {
+       next: None,
+       val
+     }
+   }
+ }
+impl Solution {
+    pub fn sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+    }
+}
