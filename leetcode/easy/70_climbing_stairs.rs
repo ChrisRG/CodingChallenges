3080ab21e196e2a30d78dd6ea08af0baea77f9d3
Chris Geekie <chris.geekie@gmail.com>
2021-06-08 14:00:09 +0200

    Finished

--- leetcode/easy/70_climbing_stairs.rs
+++ leetcode/easy/70_climbing_stairs.rs
@@ -6,13 +6,24 @@ struct Solution;
 
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-       0 
+        let mut memo: Vec<Option<usize>> = vec![None; (n as usize) + 1];
+        Solution::climb_on_up(n as usize, &mut memo) as i32
+    }
+    
+    fn climb_on_up(n: usize, memo: &mut Vec<Option<usize>>) -> usize {
+        if n == 1 || n == 0 { return 1; }
+        
+        if memo[n].is_none() {
+            memo[n] = Some(Solution::climb_on_up(n - 1, memo) + Solution::climb_on_up(n - 2, memo));
+        }
+       
+        memo[n].unwrap()
     }
 }
 
 fn main() {
-    let sol1 = Solution::climb_stairs(2);
-    let exp1 = 2;
+    let sol1 = Solution::climb_stairs(45);
+    let exp1 = 1836311903;
     println!("Result: {:?}, Expected: {}", sol1, exp1);
 
     let sol2 = Solution::climb_stairs(3);
