ed8fe00e372438846f9127116d5a27199138d6ad
Chris Geekie <chris.geekie@gmail.com>
2021-06-08 20:14:34 +0200

    Time out solution

--- leetcode/easy/877_stone_game.rs
+++ leetcode/easy/877_stone_game.rs
@@ -10,7 +10,43 @@ struct Solution;
 
 impl Solution {
     pub fn stone_game(piles: Vec<i32>) -> bool {
-        false
+        let scores: (i32, i32) = Solution::stone_fun(&piles, true);
+        return scores.0 > scores.1;
+    }
+    
+    // Considering some pointers to the vec to pass around!
+    fn stone_fun(piles: &[i32], alex_turn: bool) -> (i32, i32) {
+        if piles.len() == 2 {
+            if piles[0] > piles[1] {
+                return (piles[0], piles[1]);
+            } else {
+                return (piles[1], piles[0]);
+            }
+        }
+        if alex_turn {
+            let (alex_front, lee_front) = Solution::stone_fun(&piles[1..], false);
+            let alex_f_final = alex_front + piles[0];
+            
+            let (alex_back, lee_back) = Solution::stone_fun(&piles[..piles.len()-1], false);
+            let alex_b_final = alex_back + piles[piles.len()-1];
+            
+            if alex_f_final > alex_b_final {
+                return (alex_f_final, lee_front);
+            } else {
+                return (alex_b_final, lee_back);
+            }
+        } else {
+            let (alex_front, lee_front) = Solution::stone_fun(&piles[1..], true);
+            let lee_f_final = lee_front + piles[0];
+            let (alex_back, lee_back) = Solution::stone_fun(&piles[..piles.len()-1], true);
+            let lee_b_final = lee_back + piles[piles.len()-1];
+            if lee_f_final > lee_b_final {
+                return (alex_front, lee_f_final);
+            } else {
+                return (alex_back, lee_b_final);
+            }
+        }
+        
     }
 }
 
