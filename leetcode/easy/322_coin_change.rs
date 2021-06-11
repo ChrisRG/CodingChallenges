a6299d978e1185b35194e57c972b1cf3084d4a43
Chris Geekie <chris.geekie@gmail.com>
2021-06-11 14:24:28 +0200

    Added coin change

---
+++ leetcode/easy/322_coin_change.rs
@@ -0,0 +1,19 @@
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
+
+struct Solution;
+
+impl Solution {
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+       5 
+    }
+}
+
+fn main() {
+    let input1 = vec![1,2,5];
+    let sol = Solution::coin_change(input1, 11);
+    println!("Result: {}, Expected: 3", sol);
+}
